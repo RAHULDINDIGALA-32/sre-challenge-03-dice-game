be3da6f77a98e7ecce01b1c1f347e4db90512d8f
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-12-09 19:28:38 +0530

    feat: write the rigging logic to rig the dice game

--- packages/hardhat/contracts/RiggedRoll.sol
+++ packages/hardhat/contracts/RiggedRoll.sol
@@ -7,14 +7,34 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RiggedRoll is Ownable {
     DiceGame public diceGame;
+    
+    error RiggedRoll__LostRoll();
 
     constructor(address payable diceGameAddress) Ownable(msg.sender) {
         diceGame = DiceGame(diceGameAddress);
     }
 
+    // Include the `receive()` function to enable the contract to receive incoming Ether.
+    receive() external payable {}
+
+
     // Implement the `withdraw` function to transfer Ether from the rigged contract to a specified address.
 
     // Create the `riggedRoll()` function to predict the randomness in the DiceGame contract and only initiate a roll when it guarantees a win.
+    function riggedRoll() public {
+        require(address(this).balance >= 0.002 ether, "NotEnoughEther()");
 
-    // Include the `receive()` function to enable the contract to receive incoming Ether.
+        bytes32 prevHash = blockhash(block.number - 1);
+        bytes32 hash = keccak256(abi.encodePacked(prevHash, address(diceGame), diceGame.nonce()));
+        uint256 roll = uint256(hash) % 16;
+
+        if(roll > 5) {
+            revert RiggedRoll__LostRoll();
+        }
+
+        diceGame.rollTheDice{value: 0.002 ether}();
+
+    }
 }
+
+    
